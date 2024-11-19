44255925e593d72068cc81a72308cdf0d0a6cda5
Anastasia Kozlova <kozlova.sia@yandex.ru>
2024-11-19 16:47:52 +0100

    Initial Commit

---
+++ RickAndMortyCharacters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyCharacters
+//
+//  Created by Анастасия Козлова on 19.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
